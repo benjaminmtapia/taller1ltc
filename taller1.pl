2ab9c4f99042d769f8c9396eeb394db493d45651
Benjamín Muñoz <benjamin.munoz.tapia@gmail.com>
2019-10-01 18:20:27 -0300

    first commit

---
+++ taller1.pl
@@ -0,0 +1,63 @@
+%Base de conocimiento
+%hechos
+esFruta("cereza").
+esFruta("frutilla").
+esFruta("manzana").
+
+%fallas
+fallas("machucon").
+fallas("cicatriz").
+fallas("magulladura").
+fallas("medialuna").
+fallas("sin color").
+fallas("fruto arrugado").
+fallas("herida cicatrizada").
+fallas("perforacion cicatrizada").
+fallas("quemadura solar").
+fallas("sin pedunculo").
+
+perforacion("si").
+perforacion("no").
+
+coloracion("rosada").
+coloracion("verde").
+coloracion("roja").
+coloracion("negra").
+
+mancha("russet").
+mancha("quemadura de sol").
+
+cobertura("alto").
+cobertura("medio").
+cobertura("bajo").
+
+tamano("grande").
+tamano("medio").
+tamano("pequeno").
+
+peso("bajo").
+peso("normal").
+peso("alto").
+
+dureza("alta").
+dureza("adecuada").
+dureza("baja").
+
+textura("rugosa").
+textura("lisa").
+
+pedunculo("si").
+pedunculo("no").
+
+
+
+%machucon debe estar manchada, rugosa y blanda
+%cicatriz solo eso
+%pedunculo solo pedunculo
+%magulladura perforada
+%perforacion cicatrizada solo eso
+%fruto gemelo tiene otro fruto y arrugado
+%fruto doble peso alto, arrugado, normal
+%sin color, color rosado
+%fruto arrugado piel rugosa
+%madurez excesiva, solo eso
