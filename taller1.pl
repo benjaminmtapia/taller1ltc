8dab52e54d681f4e8be4f4fe19d11517dc09c709
Maximiliano Arévalo <maximiliano.arevalo@usach.cl>
2019-10-07 14:24:53 -0300

    Taller 1 Finalizado

--- taller1.pl
+++ taller1.pl
@@ -1,48 +1,67 @@
+%Taller 1 hecho por Maximiliano Arevalo, Valentina Ligueño, Nicolas Lopez y Benjamin Muñoz
+
+%##########################################################################################
+
 %Base de conocimiento
+
+%Determinar si una cereza tiene perforacion
 perforacion("si").
 perforacion("no").
 
+%Determinar la coloracion de una cereza
 coloracion("rosada").
 coloracion("verde").
 coloracion("roja").
 coloracion("negra").
 coloracion("purpura").
 
+%Determinar si una cereza tiene mancha
 mancha("no").
 mancha("quemadura de sol").
+mancha("russet").
 
+%Determinar la cobertura de la mancha en una cereza
 cobertura("alto").
 cobertura("medio").
 cobertura("baja").
 
+%Determinar el tamano de la cicatriz de la cereza
 tamano("grande").
 tamano("medio").
 tamano("pequeno").
 
+%Determinar el peso de la cereza
 peso("bajo").
 peso("normal").
 peso("alto").
 
+%Determinar la dureza de la cereza
 dureza("alta").
 dureza("adecuada").
 dureza("baja").
 
+%Determinar la textura de la cereza
 textura("rugosa").
 textura("lisa").
 
+%Determinar si una cereza tiene pedunculo
 pedunculo("si").
 pedunculo("no").
 
+%Determinar si una cereza tiene fruto doble
 doble("si").
 doble("no").
 
+%Determinar si una cereza tiene cicatriz
 cicatriz("si").
 cicatriz("no").
 
+%Determinar si una cereza posee partidura
 partidura("si").
 partidura("no").
 
-%calibre(salida,diametro,tamano)
+%Determinar el calibre, tamano y salida de una cereza determinada
+%De la forma calibre(salida,diametro,tamano)
 calibre(1,18,"pequeño").
 calibre(2,19,"pequeño").
 calibre(3,20,"pequeño").
@@ -60,23 +79,27 @@ calibre(14,31,"muy grande").
 calibre(15,32,"muy grande").
 calibre(16,"desecho","muy grande").
 
+%##########################################################################################
 
-%machucon debe estar manchada, rugosa y blanda LISTO
-%cicatriz solo eso                             LISTO
-%pedunculo solo pedunculo                      LISTO
-%magulladura perforada                         LISTO
-%perforacion cicatrizada solo eso              LISTO
-%fruto gemelo tiene otro fruto y arrugado      LISTO
-%fruto doble peso alto, arrugado, normal       LISTO
-%sin color, color rosado                       LISTO
-%fruto arrugado piel rugosa                    LISTO
-%madurez excesiva, solo eso                    LISTO
-%partidura cicatrizada, partido y cicatriz     LISTO
+%Supuestos para la consideracion de defectos
 
-%perforacion, color, mancha, cobertura, tamano, peso, dureza, textura, pedunculo, doble, cicatriz, parti
-%cereza(PER,COL,MAN,TAM,PES,DUR,TEX,PED,[CIC,COB],DOB,PAR,L)
+%machucon debe estar manchada, rugosa y blanda 
+%magulladura perforada                         
+%perforacion cicatrizada solo eso              
+%fruto gemelo tiene otro fruto y arrugado      
+%fruto doble peso alto, arrugado, normal       
+%sin color, color rosado                       
+%fruto arrugado piel rugosa                    
+%madurez excesiva, solo eso                    
+%partidura cicatrizada, partido y cicatriz     
+%perforacion, color, mancha, cobertura, tamano, peso, dureza, textura, pedunculo, doble, cicatriz, partidura
+%quemadura solar debe poseer una mancha por quemadura solar
 
-%CLAUSULAS DE HORN
+%Representacion de una cereza del tipo: cereza(PER,COL,MAN,TAM,PES,DUR,TEX,PED,[CIC,COB],DOB,PAR,L)
+
+%##########################################################################################
+
+%Clausulas de Horn
 
 %auxiliar para agregar elemento al final de una lista
 %fuente: https://stackoverflow.com/questions/15028831/how-do-you-append-an-element-to-a-list-in-place-in-prolog
@@ -92,58 +115,99 @@ delete_all(X, [X|Xs], Y) :-
 delete_all(X, [T|Xs], [T|Y]) :-
     dif(X, T),
     delete_all(X, Xs, Y).
-count([],0).
-count([1|T],N) :- count(T,N1), N is N1 + 1.
-count([X|T],N) :- X \= 1, count(T,N).
 
-%DETECCIÓN DE DEFECTOS
+
+%Deteccion de defectos
+
+%Entrada: Una cereza con sus respectivos parametros
+%Procedimiento: Valida los parametros que tiene la cereza
+%Salida: La cereza como tal
 cereza(PER,COL,MAN,TAM,PES,DUR,TEX,PED,CIC,DOB,PAR):- perforacion(PER), coloracion(COL),mancha(MAN), 
                                                       tamano(TAM),peso(PES), dureza(DUR), textura(TEX),
                                                       pedunculo(PED), cicatriz(CIC), doble(DOB), partidura(PAR).
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica la textura de la cereza 
+%Salida: Si la textura es rugosa, la cereza esta arrugada
 frutoArrugado(TEX,DEFECTO):- ((TEX == "rugosa") -> DEFECTO = "fruto arrugado";
                                                    DEFECTO = "no"). 
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica la dureza y la textura de la cereza
+%Salida: Si la dureza es baja y la textura es rugosa, la cereza posee un machucon
 machucon(DUR,TEX,DEFECTO):- ((DUR == "baja",
                              TEX == "rugosa") -> DEFECTO = "machucon";
                                                  DEFECTO = "no").
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica la coloracion de la cereza
+%Salida: Si la coloracion es rosada, la cereza no posee color
 sinColor(COL,DEFECTO):- ((COL == "rosada") -> DEFECTO = "sin color";
                                               DEFECTO = "no"). 
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica si una cereza posee perforacion
+%Salida: Si hay perforacion, la cereza posee una magulladura perforada
 magulladura(PER,DEFECTO):- ((PER == "si") -> DEFECTO = "magulladura perforada";
                                                         DEFECTO = "no" ).
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica el peso y la existencia de un fruto doble en una cereza
+%Salida: Si existe un fruto doble y el peso de la cereza es alto, la cereza posee un fruto doble
 frutoDoble(PES,DOB,DEFECTO):- ((DOB=="si", PES=="alto") -> DEFECTO = "fruto doble";
                                                            DEFECTO = "no").
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verificar la existencia de pedunculo en una cereza
+%Salida: Si no hay pedunculo, la cereza es sin pedunculo
 pedunculo(PED,DEFECTO):- ((PED == "si") -> DEFECTO = "sin pedunculo";
                                            DEFECTO = "no"). 
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica la textura y la existencia de fruto doble en la cereza
+%Salida: Si existe fruto doble y la textura es rugosa, la cereza tiene fruto gemelo
 frutoGemelo(TEX,DOB,DEFECTO):- ((DOB == "si",  
                                  TEX == "rugosa") -> DEFECTO = "fruto gemelo";
                                                            DEFECTO ="no").
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica la coloracion, el tamaño, el peso y la dureza de una cereza
+%Salida: Si la dureza es baja, la coloracion es negra o purpura, el tamaño es grande y su peso es alto. Entonces, la cereza tiene madurez excesiva
 madurezExcesiva(COL,TAM,PES,DUR,DEFECTO):-((dureza(DUR),DUR=="baja", (COL == "negra"; 
                                             COL == "purpura"),TAM == "grande",
                                             PES == "alto") -> DEFECTO = "madurez excesiva";
                                                               DEFECTO = "no").
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica la cobertura y la cicatriz de una cereza
+%Salida: Si la cobertura es baja y existe cicatriz, entonces la cereza tiene cicatriz
 cicatriz(COB,CIC,DEFECTO):-((COB == "baja",
                              CIC == "si") -> DEFECTO = "cicatriz";
                                              DEFECTO = "no").
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica si una cereza tiene cicatriz y partidura
+%Salida: Si tiene cicatriz y partidura, entonces la cereza tiene partidura cicatrizada
 partiduraCicatrizada(CIC,PAR,DEFECTO):- ((CIC == "si", 
                                           PAR == "si") -> DEFECTO = "partidura cicatrizada";
                                                           DEFECTO = "no").
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica si una cereza tiene cicatriz y perforacion
+%Salida: Si tiene cicatriz y perforacion, entonces la cereza tiene perforacion cicatrizada
 perforacionCicatrizada(PER,CIC,DEFECTO):- ((PER=="si",
                                             CIC=="si") -> DEFECTO="perforacion cicatrizada"; 
                                                           DEFECTO="no").
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Verifica la mancha de una cereza
+%Salida: Si la mancha es solar, entonces la cereza tiene quemadura de sol
 quemaduraSolar(MAN,DEFECTO):- ((MAN == "quemadura de sol") -> DEFECTO = "quemadura solar"; 
                                                               DEFECTO = "no").
 
+%Entrada: Parametros requeridos para la deteccion del defecto en la cereza
+%Procedimiento: Comprueba la existencia de todos los defectos para una cereza en particular, y los agrega en una lista
+%Salida: Lista con los defectos encontrados para una cereza en particular
 detectarDefectos(PER,COL,MAN,TAM,PES,DUR,TEX,PED,[CIC,COB],DOB,PAR,L):-
                                     cereza(PER,COL,MAN,TAM,PES,DUR,TEX,PED,CIC,DOB,PAR) ->
                                     (frutoArrugado(TEX,A), 
@@ -169,7 +233,9 @@ detectarDefectos(PER,COL,MAN,TAM,PES,DUR,TEX,PED,[CIC,COB],DOB,PAR,L):-
                                     quemaduraSolar(MAN,K),
                                     add_tail(L10,K,L)); write("Alguna caracteristica esta mal ingresada, verifique e intente nuevamente").
 
-
+%Entrada: Una cereza con todos sus parametros, el diametro de esta y una variable de salida
+%Procedimiento: Detecta los defectos de una cereza, y en base a su numero de defectos y tamaño retorna su linea de embalaje respectiva
+%Salida: Linea de embalaje y si la fruta es exportable, fruto comercial o desecho
 salidaEmbalaje(PER,COL,MAN,TAM,PES,DUR,TEX,PED,[CIC,COB],DOB,PAR,DIAMETRO,SALIDA):-
                   detectarDefectos(PER,COL,MAN,TAM,PES,DUR,TEX,PED,[CIC,COB],DOB,PAR,L),
                   length(L,CANTIDADDEFECTOS),
